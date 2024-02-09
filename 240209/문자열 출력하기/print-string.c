f5c9b03522c79934c112b3b7ddffa4260a42dc30
I-yunA <157999381+I-yunA@users.noreply.github.com>
2024-02-09 19:20:17 +0900

    Title: 문자열 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240209/문자열 출력하기/print-string.c
+++ 240209/문자열 출력하기/print-string.c
@@ -4,8 +4,11 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    for(int i = 1; i <= n; i++)
+    int i = 1;
+    while(i <= n) {
         printf("LeebrosCode\n");
-        
+        i++;
+    }
+
     return 0;
 }
