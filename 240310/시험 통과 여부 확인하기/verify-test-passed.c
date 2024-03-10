df3d6100dcd4e6ca8f8a7107a027dbad31014127
I-yunA <157999381+I-yunA@users.noreply.github.com>
2024-03-10 16:14:48 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240310/시험 통과 여부 확인하기/verify-test-passed.c
+++ 240310/시험 통과 여부 확인하기/verify-test-passed.c
@@ -4,10 +4,10 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    if(n >= 80)
+    if(n>=80)
         printf("pass");
     else
-        printf("%d more score", 80 - n);
-
+        printf("%d more score", 80-n);
+            
     return 0;
 }
