9ef0b0a476e510c8f0cfe0811100881cb03bb798
I-yunA <157999381+I-yunA@users.noreply.github.com>
2024-03-11 11:27:54 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240311/삼항연산자/ternary-operator.c
+++ 240311/삼항연산자/ternary-operator.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    printf((n==100)? "pass": "failure");
-    
+    char r[8];
+
+    (n==100)? strcpy(r, "pass") : strcpy(r, "failure");
+
+    printf("%s", r);
+
     return 0;
 }
