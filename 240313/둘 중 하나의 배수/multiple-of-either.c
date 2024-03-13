4bf34f0d14c4d72b35ede95d3e9065d59b406473
I-yunA <157999381+I-yunA@users.noreply.github.com>
2024-03-13 19:46:07 +0900

    Title: 둘 중 하나의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240313/둘 중 하나의 배수/multiple-of-either.c
+++ 240313/둘 중 하나의 배수/multiple-of-either.c
@@ -4,7 +4,9 @@ int main() {
     int a;
     scanf("%d", &a);
 
-    printf("%d", a % 3 == 0 || a % 5 == 0);    
-            
+    //printf("%d", a % 3 == 0 || a % 5 == 0);    
+
+    printf((a % 3 == 0 || a % 5 == 0)? "1" : "0");
+
     return 0;
 }
