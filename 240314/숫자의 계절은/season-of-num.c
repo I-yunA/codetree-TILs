465f8d09f8157477ee844fcbf043bd29178cac4d
I-yunA <157999381+I-yunA@users.noreply.github.com>
2024-03-14 17:12:12 +0900

    Title: 숫자의 계절은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240314/숫자의 계절은/season-of-num.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int m;
+    scanf("%d",&m);
+
+    if(m==12 || m <= 2)
+        printf("Winter");
+    else if(m >= 9)
+        printf("Fall");
+    else if(m >= 6)
+        printf("Summer");
+    else
+        printf("Spring");
+
+    return 0;
+}
