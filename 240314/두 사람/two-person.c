e4492b21ee9c95b04d54836a14a5a7735b850fd1
I-yunA <157999381+I-yunA@users.noreply.github.com>
2024-03-14 18:14:45 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240314/두 사람/two-person.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int age1, age2;
+    char gender1, gender2;
+
+    scanf("%d %c", &age1, &gender1);
+    scanf("%d %c", &age2, &gender2);
+
+    if((age1 >= 19 && gender1 == 'M') || (age2 >= 19 && gender2 == 'M'))
+        printf("1");
+    else
+        printf("0");
+            
+    return 0;
+}
