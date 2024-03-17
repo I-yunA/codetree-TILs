8d573818395384ec938adcfbb931316e21b35ee2
I-yunA <157999381+I-yunA@users.noreply.github.com>
2024-03-17 15:48:35 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240317/윤년인가/is-leap-year.c
+++ 240317/윤년인가/is-leap-year.c
@@ -4,10 +4,18 @@ int main() {
     int y;
     scanf("%d", &y);
 
-    if((y % 4 == 0 && y % 100 != 0)||(y % 400 == 0))
+    if(y % 4 == 0)
+    {
+        if(y % 100 != 0)
+            printf("true");
+        else
+            printf("false");    
+    }
+    else if(y % 400 == 0)
         printf("true");
+
     else
         printf("false");
-            
+
     return 0;
 }
