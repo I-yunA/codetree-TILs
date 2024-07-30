8a6dc6216dc21eed7caa11e53ade6fb68df71fb7
I-yunA <157999381+I-yunA@users.noreply.github.com>
2024-07-30 10:34:23 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240730/청소당번 정하기/cleaning-numbering.c
+++ 240730/청소당번 정하기/cleaning-numbering.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int a,b,c;
     int n;
+    scanf("%d", &n);//한번만 입력
+
+    int a=0, b=0, c=0;//초기화
 
     for(int i=1; i<=n; i++)
     {
-        scanf("%d", &n);
-        
-        if(a%2==0)
-            a++;
-        if(b%3==0)
+        if(i%12==0)// 주기가 긴 것부터//i값이 증가하면서
+            c++; //카운트 변수
+        else if(i%3==0)//겹치면 안되서 
             b++;
-        if(c%12==0)
-            c++;
+        else if(i%2==0)
+            a++;
     }
     printf("%d %d %d", a, b, c);
     return 0;
