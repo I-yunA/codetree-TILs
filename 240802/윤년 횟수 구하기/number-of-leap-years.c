63cb11723e6772220c3537e13d9674f09aafd0a5
I-yunA <157999381+I-yunA@users.noreply.github.com>
2024-08-02 17:24:51 +0900

    Title: 윤년 횟수 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240802/윤년 횟수 구하기/number-of-leap-years.c
+++ 240802/윤년 횟수 구하기/number-of-leap-years.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int n, cnt1, cnt2;
+    int n, cnt1;
     cnt1=0;
-    cnt2=0;
 
     for(int i=1; i<=n; i++) 
     {
         if(i%400==0)
             cnt1++;            
-        else if(i%100==0)
-            cnt2++;
+        else if(i%100!=0)
+            cnt1++;
         else if(i%4==0)
             cnt1++;
-        else
-            cnt2++;
     }
     printf("%d", cnt1);
     return 0;
